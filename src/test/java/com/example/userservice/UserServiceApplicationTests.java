cfac5d4b8f2373ecad494347ec4432f0ac7d96da
jbg <jbg4354@naver.com>
2025-07-25 13:32:18 +0900

    update test

--- src/test/java/com/example/userservice/UserServiceApplicationTests.java
+++ src/test/java/com/example/userservice/UserServiceApplicationTests.java
@@ -2,10 +2,20 @@ package com.example.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import com.example.userservice.jwt.JwtUtil;
 
 @SpringBootTest
 class UserServiceApplicationTests {
 
+	@MockBean
+	private RedisTemplate<String, String> redisTemplate;
+	
+	@MockBean
+	private JwtUtil jwtUtil; 
+	
 	@Test
 	void contextLoads() {
 	}
